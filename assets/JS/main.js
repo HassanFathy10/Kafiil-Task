056f42fb97e76012dc16947b6f48426b5d484f3f
Hassan Fathy <118758238+HassanFathy10@users.noreply.github.com>
2024-01-09 21:16:42 +0200

    .

---
+++ assets/JS/main.js
@@ -0,0 +1,13 @@
+// Build menu 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar-list");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("navbar-link").forEach(link =>
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
