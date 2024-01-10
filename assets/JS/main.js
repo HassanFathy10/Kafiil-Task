2bf90643d761008dbf750c1d75eef8ffbfeccad3
Hassan Fathy <118758238+HassanFathy10@users.noreply.github.com>
2024-01-10 08:03:56 +0200

    .

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -44,15 +44,3 @@ const toggleWishlist = (event) => {
 };
 
 // swiper 
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 5,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
