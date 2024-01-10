34b4c32f6f08de1e4d7d12cccfe1ad42fe9b3b2b
Hassan Fathy <118758238+HassanFathy10@users.noreply.github.com>
2024-01-10 19:52:29 +0200

    .

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -44,3 +44,19 @@ const toggleWishlist = (event) => {
 };
 
 // swiper 
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
